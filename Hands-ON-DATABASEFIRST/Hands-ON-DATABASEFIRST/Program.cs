6cc0609f732f54aad25de06dab7a357df6fe476b
Krishna143-cog <68010445+Krishna143-cog@users.noreply.github.com>
2020-07-21 15:25:33 +0530

    Add files via upload

---
+++ Hands-ON-DATABASEFIRST/Hands-ON-DATABASEFIRST/Program.cs
@@ -0,0 +1,33 @@
+﻿using Hands_ON_DATABASEFIRST;
+using System;
+using System.Linq;
+
+namespace DatabaseFirstDemo
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            using (var db = new BookStoreEntities())
+            {
+
+                var query = from b in db.Books
+                            orderby b.Title
+                            select b;
+
+                Console.WriteLine("All books in the database:");
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Title);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
